edb3488b9a1a9926135badc458d24cab29999199
Eduardo Salarini <luis.salarini@aluno.cefet-rj.br>
2024-07-23 16:25:24 -0300

    att

--- conexao100/settings.py
+++ conexao100/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'conexao100.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-}
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
