305f33ea286d0f85b16e0add8252e491fc2cd06b
Eduardo Salarini <luis.salarini@aluno.cefet-rj.br>
2024-07-23 16:24:10 -0300

    att

--- conexao100/settings.py
+++ conexao100/settings.py
@@ -63,10 +63,20 @@ WSGI_APPLICATION = 'conexao100.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'conexao100',      
+        'USER': 'conexao100',
+        'PASSWORD': 'conexao100admin2264@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
